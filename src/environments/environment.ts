aabe3d5f02abb685f8ffa9a76a6533633f570da7
Wellington P Bastos <wpbastos@live.com>
2020-06-03 13:08:05 -0400

    Adjusted do build --prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiURL: 'http://localhost:3000/api/',
-  apiURL: 'https://wpb-node-post.herokuapp.com/api/',
+  // apiURL: 'https://wpb-node-post.herokuapp.com/api/',
 };
 
 /*
